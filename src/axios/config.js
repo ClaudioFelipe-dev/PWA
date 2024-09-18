f5931a7b0d07430d9d7255df334993708d514347
felipe <claudiofmsilva17@gmail.com>
2024-09-18 15:52:54 -0300

    Editando arquivo config

--- src/axios/config.js
+++ src/axios/config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const partyFetch = axios.create({
-  baseURL: "http://localhost:3000/api-pwa",
+  baseURL: "https://api-dog-kappa.vercel.app/",
   headers: {
     "Content-Type": "application/json",
   },
