fcd7531d1bdc36004289030f80e2afbc84822acd
felipe <claudiofmsilva17@gmail.com>
2024-09-19 16:59:44 -0300

    Editando axios

--- src/axios/config.js
+++ src/axios/config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const partyFetch = axios.create({
-  baseURL: "https://api-henna-five-66.vercel.app/",
+  baseURL: "https://api-1-wjn8.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
